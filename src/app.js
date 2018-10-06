d4bd517fed180e426cccc9552d0bd3e4890a16b0
eychcue <hq.haroon1@gmail.com>
2018-10-05 21:46:18 -0400

    react boilerplate

---
+++ src/app.js
@@ -0,0 +1,12 @@
+console.log('App.js is running!');
+
+var template = (
+    <div>
+        <p>This is a sample post!!!</p>
+
+    </div>
+    );
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
