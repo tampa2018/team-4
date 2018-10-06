7b21bfeb5a30e67bf26e01d24eeb0eb2bd91aeb7
Tirtha Subedi <subedi.tirtha@gmail.com>
2018-10-06 04:49:49 -0400

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-
+//using express to connect with mysql
 const app = express();
 
 // Point static path to dist
