c1683933e2cd7f37bc054ee67ff3c6ee8e030c39
Michelle Li <michelleli94237@gmail.com>
2018-10-06 02:00:40 -0400

    loading database

---
+++ database/loading_database.sql
@@ -0,0 +1,11 @@
+INSERT INTO Users
+VALUES(2, "mary2", "shelley", "target", "something@jpmorggan.com");
+
+INSERT INTO Ideas
+VALUES(1, 'hello', 'hello', 1, DATE ('2017-06-15'), 1, 1, 1, 1);
+
+INSERT INTO Ideas
+VALUES(2, 'hello', 'hello', 1, CURDATE(), 1, 1, 1, 2);
+
+INSERT INTO Ideas
+VALUES(3, 'helloooo', 'hello', 1, CURDATE(), 1, 1, 1, 2);
