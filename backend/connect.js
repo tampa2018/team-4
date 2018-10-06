90af500411635de5a9bfd85d4f88eec9aa16478f
JessZam <38445173+JessZam@users.noreply.github.com>
2018-10-06 06:15:28 -0400

    Update connect.js

--- backend/connect.js
+++ backend/connect.js
@@ -40,7 +40,14 @@ app.get('/ideas', (req, res) => {
     })
 });
 
-
+app.get('/comments', (req, res) => {
+    mysqlConnection.query('SELECT * FROM Comments', (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+});
 
 //create a listener
 app.listen(8000, () => {
