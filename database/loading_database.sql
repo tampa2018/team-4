79ee9429611512d2138dcca3086b0e6a5a24eeb6
Michelle Li <michelleli94237@gmail.com>
2018-10-06 02:50:00 -0400

    updating with more test data

--- database/loading_database.sql
+++ database/loading_database.sql
@@ -1,11 +1,41 @@
 INSERT INTO Users
-VALUES(2, "mary2", "shelley", "target", "something@jpmorggan.com");
+VALUES(1, "matt", "shelley", "target", "something@jpmorggan.com");
+
+INSERT INTO Users
+VALUES(2, "kary", "anello", "target", "something@jpmorggan.com");
+
+INSERT INTO Users
+VALUES(3, "tary", "neal", "target", "something@jpmorggan.com");
+
+INSERT INTO Users
+VALUES(4, "amy", "shelley", "target", "something@jpmorggan.com");
+
+INSERT INTO Users
+VALUES(5, "ash", "james", "target", "something@jpmorggan.com");
+
+INSERT INTO Users
+VALUES(6, "moe", "shaun", "target", "something@jpmorggan.com");
+
+INSERT INTO Users
+VALUES(7, "mohammed", "joanne", "target", "something@jpmorggan.com");
+
+INSERT INTO Users
+VALUES(8, "joe", "green", "target", "something@jpmorggan.com");
 
 INSERT INTO Ideas
-VALUES(1, 'hello', 'hello', 1, DATE ('2017-06-15'), 1, 1, 1, 1);
+VALUES(1, 'weather climate', 'weather climate is not a concern', 1, DATE ('2018-06-15'), 1, 1, 1, 1);
 
 INSERT INTO Ideas
-VALUES(2, 'hello', 'hello', 1, CURDATE(), 1, 1, 1, 2);
+VALUES(2, 'global warming', 'the weather is unstable due to...', 1, CURDATE(), 1, 1, 1, 2);
 
 INSERT INTO Ideas
-VALUES(3, 'helloooo', 'hello', 1, CURDATE(), 1, 1, 1, 2);
+VALUES(3, 'water levels rising', 'the reason for the high water levels are due to...', 1, CURDATE(), 1, 1, 1, 3);
+
+INSERT INTO Ideas
+VALUES(4, 'hurricane damage', 'what is causing this damage?', 1, DATE ('2018-06-15'), 1, 1, 1, 1);
+
+INSERT INTO Ideas
+VALUES(5, 'rain season', 'why does it rain so often? the reason is due to the flying pigeons.", 1, CURDATE(), 1, 1, 1, 8);
+
+INSERT INTO Ideas
+VALUES(6, 'don't care', 'This is totally irrelatant to us...', 1, CURDATE(), 1, 1, 1, 8);
